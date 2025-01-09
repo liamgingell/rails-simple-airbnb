4f80269589b96c226d70b679e06a554f67a89d3a
Liam Gingell <elgee95@gmail.com>
2025-01-09 20:02:13 +0900

    Added view for index and show

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,15 @@
 class Flat < ApplicationRecord
+  def to_s
+    name
+  end
+
+  validates :name, :address, :description, :price_per_night, :number_of_guests, presence: true
+
+  validates :price_per_night, numericality: { greater_than: 0 }
+
+  validates :number_of_guests, numericality: { greater_than: 0 }
+
+  validates :name, uniqueness: true
+
+  validates :description, length: { minimum: 25 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Stylish House Close to River Thames',
+  address: '5 Riverside London E1 1AA',
+  description: 'A modern house with a beautiful view of the River Thames. Three bedrooms, spacious living area, and a fully equipped kitchen',
+  price_per_night: 100,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Cozy Apartment in Central London',
+  address: '22 Baker Street London NW1 6XE',
+  description: 'A cozy apartment located in the heart of London. One bedroom, a comfortable living area, and a small kitchen',
+  price_per_night: 60,
+  number_of_guests: 2
+)
